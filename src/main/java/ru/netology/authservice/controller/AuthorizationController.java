2a920b784f7aa88da5ed108b9049917fed849afe
Dmitriy Ozhgibesov <dmitry.ozhghibesov@innowave.ai>
2023-06-08 12:30:39 +0700

    Exception handlers fixed

--- src/main/java/ru/netology/authservice/controller/AuthorizationController.java
+++ src/main/java/ru/netology/authservice/controller/AuthorizationController.java
@@ -1,9 +1,14 @@
 package ru.netology.authservice.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import ru.netology.authservice.model.Authorities;
+import ru.netology.authservice.model.InvalidCredentials;
+import ru.netology.authservice.model.UnauthorizedUser;
 import ru.netology.authservice.service.AuthorizationService;
 
 import java.util.List;
@@ -19,5 +24,25 @@ public class AuthorizationController {
     @GetMapping("/authorize")
     public List<Authorities> getAuthorities(@RequestParam("user") String user, @RequestParam("password") String password) {
         return service.getAuthorities(user, password);
+    }
+
+    @ExceptionHandler(UnauthorizedUser.class)
+    public ResponseEntity<String> unauthHandler(UnauthorizedUser e) throws JSONException {
+        JSONObject resp = new JSONObject();
+        resp.put("status", HttpStatus.UNAUTHORIZED);
+        resp.put("body", e.getMessage());
+        System.out.println("InvalidCredentials exception! Reason: " + e.getMessage());
+
+        return new ResponseEntity<>(resp.toString(), HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(InvalidCredentials.class)
+    public ResponseEntity<String> brHandler(InvalidCredentials e) throws JSONException {
+        JSONObject resp = new JSONObject();
+        resp.put("status", HttpStatus.BAD_REQUEST);
+        resp.put("body", e.getMessage());
+        System.out.println("InvalidCredentials exception! Reason: " + e.getMessage());
+
+        return new ResponseEntity<>(resp.toString(), HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/ru/netology/authservice/model/InvalidCredentials.java
+++ src/main/java/ru/netology/authservice/model/InvalidCredentials.java
@@ -1,14 +1,7 @@
 package ru.netology.authservice.model;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
-
 public class InvalidCredentials extends RuntimeException {
     public InvalidCredentials(String msg) {
-        try {
-            throw new RuntimeException(msg);
-        } catch (RuntimeException ex) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, msg, ex);
-        }
+        super(msg);
     }
 }
